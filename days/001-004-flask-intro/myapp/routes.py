55637b96c0bcc7d9b7f742f29d3aadb15d34a081
seunodewale <seunodewale.zone5@gmail.com>
2020-04-27 13:50:55 +0100

    add workdone from day1-4

---
+++ days/001-004-flask-intro/myapp/routes.py
@@ -0,0 +1,13 @@
+from myapp import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
